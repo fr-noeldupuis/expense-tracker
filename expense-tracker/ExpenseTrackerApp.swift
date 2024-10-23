4d4339c5c5ae6a10c04b947632fd9b18791c8bb7
Noel Dupuis <noel.dupuis45370@gmail.com>
2024-10-23 17:10:44 +0200

    Add .gitignore file

---
+++ expense-tracker/ExpenseTrackerApp.swift
@@ -0,0 +1,18 @@
+//
+//  expense_trackerApp.swift
+//  expense-tracker
+//
+//  Created by Noel Dupuis on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct expense_trackerApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
